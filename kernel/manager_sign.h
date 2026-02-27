c4bc8b27a82a875342180bc75ff547138b3b2aca
littlepe <565903071@qq.com>
2026-02-28 00:37:57 +0800

    Add MamboSU manager signature and improve comments
    - Add MamboSU entry in manager_sign.h with its size and hash
    - Include MamboSU in the list of supported managers in Kbuild
    - Add corresponding signature entry in apk_sign.c
    - Enhance comments for all manager signatures to clearly indicate
      the associated manager name (Official KSU, MKSU, RKSU, KowSU, MamboSU,
      KernelSU-Next, Wild SU, SukiSU-Ultra, ReSukiSU)
    
    Signed-off-by: littlepe <565903071@qq.com>

--- kernel/manager_sign.h
+++ kernel/manager_sign.h
@@ -1,42 +1,47 @@
 #ifndef MANAGER_SIGN_H
 #define MANAGER_SIGN_H
 
-// weishu/KernelSU
+// weishu/KernelSU - Official KSU
 #define EXPECTED_SIZE_WEISHU 0x033b
 #define EXPECTED_HASH_WEISHU                                                   \
     "c371061b19d8c7d7d6133c6a9bafe198fa944e50c1b31c9d8daa8d7f1fc2d2d6"
 
-// 5ec1cff/KernelSU
+// 5ec1cff/KernelSU - MKSU
 #define EXPECTED_SIZE_5EC1CFF 384
 #define EXPECTED_HASH_5EC1CFF                                                  \
     "7e0c6d7278a3bb8e364e0fcba95afaf3666cf5ff3c245a3b63c8833bd0445cc4"
 
-// rsuntk/KernelSU
+// rsuntk/KernelSU - RKSU
 #define EXPECTED_SIZE_RSUNTK 0x396
 #define EXPECTED_HASH_RSUNTK                                                   \
     "f415f4ed9435427e1fdf7f1fccd4dbc07b3d6b8751e4dbcec6f19671f427870b"
 
-// KowX712/KernelSU
+// KowX712/KernelSU - KowSU
 #define EXPECTED_SIZE_KOWX712 0x375
 #define EXPECTED_HASH_KOWX712                                                  \
 	"484fcba6e6c43b1fb09700633bf2fb4758f13cb0b2f4457b80d075084b26c588"
 
-// rifsxd/KernelSU-Next
+// RapliVx/KernelSU - MamboSU
+#define EXPECTED_SIZE_RAPLI 0x384
+#define EXPECTED_HASH_RAPLI                                                   \
+    "a9462b8b98ea1ca7901b0cbdcebfaa35f0aa95e51b01d66e6b6d2c81b97746d8"
+
+// rifsxd/KernelSU-Next - KernelSU-Next
 #define EXPECTED_SIZE_RIFSXD 0x3e6
 #define EXPECTED_HASH_RIFSXD                                                   \
     "79e590113c4c4c0c222978e413a5faa801666957b1212a328e46c00c69821bf7"
 
-// WildKernels/Wild_KSU
+// WildKernels/Wild_KSU - Wild SU
 #define EXPECTED_SIZE_WILDKERNELS 0x381
 #define EXPECTED_HASH_WILDKERNELS                                              \
     "52d52d8c8bfbe53dc2b6ff1c613184e2c03013e090fe8905d8e3d5dc2658c2e4"
 
-// SukiSU-Ultra/SukiSU-Ultra
+// SukiSU-Ultra/SukiSU-Ultra - SukiSU-Ultra
 #define EXPECTED_SIZE_SHIRKNEKO 0x35c
 #define EXPECTED_HASH_SHIRKNEKO                                                \
     "947ae944f3de4ed4c21a7e4f7953ecf351bfa2b36239da37a34111ad29993eef"
 
-// ReSukiSU/ReSukiSU
+// ReSukiSU/ReSukiSU - ReSukiSU
 #define EXPECTED_SIZE_RESUKISU 0x377
 #define EXPECTED_HASH_RESUKISU                                                 \
     "d3469712b6214462764a1d8d3e5cbe1d6819a0b629791b9f4101867821f1df64"
