f720e6c0cbbb25a244396b2dfa9e39fba88ced93
littlepe <565903071@qq.com>
2026-09-20 00:01:47 +0800

    Sync upstream updates and remove some useless information
    Signed-off-by: littlepe <565903071@qq.com>

--- kernel/feature/sucompat.c
+++ kernel/feature/sucompat.c
@@ -33,6 +33,7 @@
 #include "runtime/ksud.h"
 #include "feature/sucompat.h"
 #include "policy/app_profile.h"
+#include "supercall/supercall.h"
 #ifdef CONFIG_KSU_TRACEPOINT_HOOK
 #include "hook/syscall_hook.h"
 #else
@@ -235,6 +236,7 @@ static long ksu_handle_execve_sucompat_common_internal(const char __user **filen
     char path[sizeof(su_path) + 1];
     long ret, orig_regs[5];
     unsigned long addr;
+    int su_fd = -1;
     int tmp_fd;
     struct file *ksud_file;
     const struct cred *old_cred;
@@ -309,6 +311,13 @@ static long ksu_handle_execve_sucompat_common_internal(const char __user **filen
         regs->__PT_PARM3_REG = orig_regs[2];
         regs->__PT_SYSCALL_PARM4_REG = orig_regs[3];
         regs->__PT_PARM5_REG = orig_regs[4];
+    } else {
+        // Only grant the scoped driver capability after the selected root
+        // profile has been applied successfully.
+        su_fd = ksu_install_su_fd();
+        if (su_fd < 0) {
+            pr_warn("install su session fd failed: %d\n", su_fd);
+        }
     }
     return ret;
 
@@ -343,19 +352,19 @@ static inline int do_ksu_handle_execveat_sucompat(int *fd, const char *filename,
     // Yep, maybe someusers love turn off sucompat <- idk how they managed to keep using it
     // But for mostly users, sucompat is enabled, so unlikely here
     if (!static_branch_unlikely(&ksu_su_compat_enabled)) {
-        return 0;
+        return -EINVAL;
     }
 #else
     if (!ksu_su_compat_enabled) {
-        return 0;
+        return -EINVAL;
     }
 #endif
 
     if (!is_allowed)
-        return 0;
+        return -EINVAL;
 
     if (likely(memcmp(filename, su_path, sizeof(su_path))))
-        return 0;
+        return -EINVAL;
 
     pr_info("do_execveat_common su found\n");
 
@@ -375,6 +384,24 @@ static inline int do_ksu_handle_execveat_sucompat(int *fd, const char *filename,
     memcpy((void *)filename, ksud_path, sizeof(ksud_path));
 out:
     ksu_sulog_emit_pending(pending_sucompat, 0, GFP_KERNEL);
+    // always flag that, to avoid old version of susfs hang in boot
+    set_thread_flag(TIF_PROC_IN_KSU_EXECVE);
+    return 0;
+}
+
+// fd, filename, argv, envp, flags and retval were NOT provided in bprm_committed_creds (KSU_COMPAT_NO_POST_EXECVE_HOOK)!
+int ksu_handle_post_execve(int *fd, const char *filename, void *argv, void *envp, int *flags, int *retval)
+{
+    if (likely(!test_thread_flag(TIF_PROC_IN_KSU_EXECVE))) {
+        return -EINVAL;
+    }
+#ifndef KSU_COMPAT_HAS_SUSFS_INSTALL_SU_FD_DIRECT_CALL
+    ksu_install_su_fd();
+#endif
+    // #ifdef KSU_COMPAT_NO_POST_EXECVE_HOOK
+    //     return 0;
+    // #endif
+    // TODO Implement tmpfd of ksud when KSU_COMPAT_NO_POST_EXECVE_HOOK is not defined
     return 0;
 }
 
@@ -397,11 +424,6 @@ static inline void ksu_handle_execveat_init(const char *filename, void *envp)
 
             if (!ksu_is_current_proc_unprivillege())
                 ksu_set_current_proc_unprivillege();
-
-#ifdef CONFIG_KSU_SUSFS
-            if (!susfs_is_current_proc_umounted())
-                susfs_set_current_proc_umounted();
-#endif
         }
 #endif
         int ret = ksu_adb_root_handle_execve_manual(filename, (struct user_arg_ptr *)envp);
@@ -417,7 +439,7 @@ int ksu_handle_execve(int *fd, const char *filename, void *argv, void *envp, int
 
 #ifndef CONFIG_KSU_TRACEPOINT_HOOK
     if (ksu_is_current_proc_unprivillege()) {
-        return 0;
+        return -EINVAL;
     }
 #endif
 
@@ -429,7 +451,7 @@ int ksu_handle_execve(int *fd, const char *filename, void *argv, void *envp, int
     }
 
     if (*fd != AT_FDCWD || *flags != 0) {
-        return 0;
+        return -EINVAL;
     }
 
 skip_check:
@@ -458,21 +480,29 @@ skip_check:
     return ret;
 }
 
-// old hook, link to ksu_handle_execve
 int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv, void *envp, int *flags)
 {
     struct filename *filename;
     filename = *filename_ptr;
     if (IS_ERR(filename)) {
-        return 0;
+        return -EINVAL;
     }
 
     return ksu_handle_execve(fd, filename->name, argv, envp, flags);
 }
 
-// because simonpunk, he do check in hook side
-// and call ksu_handle_execveat_sucompat
-// we need unpack filename* in here, and pass it to ksu_handle_execveat
+int ksu_handle_post_execveat(int *fd, struct filename **filename_ptr, void *argv, void *envp, int *flags, int *retval)
+{
+    struct filename *filename;
+    filename = *filename_ptr;
+    if (IS_ERR(filename)) {
+        return -EINVAL;
+    }
+
+    return ksu_handle_post_execve(fd, filename->name, argv, envp, flags, retval);
+}
+
+// compat for check in hook
 #ifdef CONFIG_KSU_SUSFS
 int ksu_handle_execveat_sucompat(int *fd, struct filename **filename_ptr, void *argv, void *envp, int *flags)
 {
@@ -484,9 +514,47 @@ int ksu_handle_execveat_sucompat(int *fd, struct filename **filename_ptr, void *
 
     return ksu_handle_execveat(fd, filename_ptr, argv, envp, flags);
 }
+
+int ksu_handle_post_execveat_sucompat(int *fd, struct filename **filename_ptr, void *argv, void *envp, int *flags,
+                                      int *retval)
+{
+    return ksu_handle_post_execveat(fd, filename_ptr, argv, envp, flags, retval);
+}
 #endif
 #endif
 
+#ifdef CONFIG_KSU_SUSFS
+int ksu_handle_faccessat(int *dfd, struct filename **filename, int *mode, int *__unused_flags)
+{
+    const struct cred *old_cred;
+
+    // we no need harden this check, susfs already complete in caller
+    // if (ksu_is_current_proc_unprivillege()) {
+    //     return 0;
+    // }
+
+    if (!static_branch_unlikely(&ksu_su_compat_enabled)) {
+        return 0;
+    }
+
+    if (unlikely(IS_ERR(*filename) || (*filename)->name == NULL))
+        return 0;
+
+    if (likely(memcmp((*filename)->name, su_path, sizeof(su_path))))
+        return 0;
+
+    old_cred = override_creds(ksu_cred);
+    if (is_ksud_exists()) {
+        pr_info("ksu_handle_faccessat su->sh!\n");
+        memcpy((void *)((*filename)->name), sh_path, sizeof(sh_path));
+    } else {
+        pr_info("no ksud found, don't process faccessat for su!");
+    }
+
+    revert_creds(old_cred);
+    return 0;
+}
+#else
 int ksu_handle_faccessat(int *dfd, const char __user **filename_user, int *mode, int *__unused_flags)
 {
     char path[sizeof(su_path) + 1] = { 0 };
@@ -499,8 +567,6 @@ int ksu_handle_faccessat(int *dfd, const char __user **filename_user, int *mode,
 #endif
 
 #ifdef KSU_COMPAT_USE_STATIC_KEY
-    // Yep, maybe someusers love turn off sucompat <- idk how they managed to keep using it
-    // But for mostly users, sucompat is enabled, so unlikely here
     if (!static_branch_unlikely(&ksu_su_compat_enabled)) {
         return 0;
     }
@@ -529,18 +595,18 @@ int ksu_handle_faccessat(int *dfd, const char __user **filename_user, int *mode,
 
     return 0;
 }
+#endif
 
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(6, 1, 0) && defined(CONFIG_KSU_SUSFS)
+#ifdef CONFIG_KSU_SUSFS
 int ksu_handle_stat(int *dfd, struct filename **filename, int *flags)
 {
     const struct cred *old_cred;
 
-    if (ksu_is_current_proc_unprivillege()) {
-        return 0;
-    }
+    // we no need harden this check, susfs already complete in caller
+    // if (ksu_is_current_proc_unprivillege()) {
+    //     return 0;
+    // }
 
-    // Yep, maybe someusers love turn off sucompat <- idk how they managed to keep using it
-    // But for mostly users, sucompat is enabled, so unlikely here
     if (!static_branch_unlikely(&ksu_su_compat_enabled)) {
         return 0;
     }
